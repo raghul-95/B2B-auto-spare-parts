36171e54c07406e00f3a4fc1ddcf4aa9829d79f4
raghul-95 <71816552+raghul-95@users.noreply.github.com>
2025-07-04 01:05:15 +0400

    app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ import base64, warnings
 warnings.filterwarnings('ignore')
 
 # ---------------- CONFIG -----------------
-DEFAULT_URL = "https://raw.githubusercontent.com/<user>/<repo>/main/Data_Analysis_R_Survey_Enhanced.csv"
+DEFAULT_URL = "https://raw.githubusercontent.com/raghul-95/B2B-auto-spare-parts/refs/heads/main/Data_Analysis_R_Survey_Enhanced.csv"
 st.set_page_config(page_title="Auto‑Parts Analytics Dashboard", layout="wide")
 
 # ------------- Data Loader ----------------
