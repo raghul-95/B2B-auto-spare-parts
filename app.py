b284eea67f1bd19eade078a7e1b283a46ee3b951
raghul-95 <71816552+raghul-95@users.noreply.github.com>
2025-07-04 01:42:34 +0400

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ pio.templates.default = "plotly_dark"
 warnings.filterwarnings("ignore")
 
 # ───────────────────────────────────────── CONFIG
-DEFAULT_URL = "https://raw.githubusercontent.com/<user>/<repo>/main/Data_Analysis_R_Survey_Enhanced.csv"
+DEFAULT_URL = "https://raw.githubusercontent.com/raghul-95/B2B-auto-spare-parts/refs/heads/main/Data_Analysis_R_Survey_Enhanced.csv"
 st.set_page_config(page_title="Auto-Parts Analytics Dashboard", layout="wide")
 
 # ──────────────────────────────────────── Data loader
